5a255dd08ef4dafc1407a1ceb6987ce275916e78
Shreyansh Nahata <shreyans4nahata@gmail.com>
2016-07-31 22:04:53 +0530

    Merged anomaly1

---
+++ anomalyFinder/requestHits.py
@@ -0,0 +1,53 @@
+import csv
+import collections
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
+
+inputCSVFile = '../parsedOutput/Parsed_localhost_access_log-1.csv' # file path
+fileObject = open(inputCSVFile)
+reader = csv.DictReader(fileObject)
+
+
+timestamp = [] # list to store all timestamps includes duplicates
+ip = [] # list to store all remote ip's
+
+# storing timestamp and ip's in a list
+for row in reader:
+    timestamp.append(row['f1'])
+    ip.append(row['f2'])
+
+unique_ip = list(set(ip)) # contains unique ip's
+
+unique_ip_len = len(unique_ip)
+
+# plotting of request hits against timestamps
+for k in range(unique_ip_len):
+    temp_dict = {} # temporary dictinary
+    final_dict = {}
+    tr = [] # list containing request hits at particular ip
+    t_list = [] #list containg formatted timestamp
+
+    # initialization of request hit to 0 corresponding timestamp
+    for j in range(len(timestamp)):
+        temp_dict[timestamp[j]] = 0
+
+    # incrementing the count for each ip
+    for j in range(len(timestamp)):
+        if ip[j] == unique_ip[k]:
+            temp_dict[timestamp[j]]+=1
+
+    # sorting dictionary by keys
+    od = collections.OrderedDict(sorted(temp_dict.items()))
+
+    for t,i in od.iteritems():
+        time,dis =t.split(' ')
+        time_stamp = datetime.datetime.strptime(time,'%d/%b/%Y:%H:%M:%S')
+        time_val = mdates.date2num(time_stamp)
+        t_list.append(time_val)
+        tr.append(i)
+
+    plt.plot(t_list,tr,'b')
+    plt.ylabel('time',fontsize = 20)
+    plt.ylabel(unique_ip[k],fontsize = 18)
+    plt.show()
