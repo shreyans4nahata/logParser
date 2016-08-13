42f3a3ede4d72e84755472abfcf33a5596a04c19
Shreyansh Nahata <shreyans4nahata@gmail.com>
2016-08-13 13:54:31 +0530

    MAD 1st

--- anomalyFinder/requestHits.py
+++ anomalyFinder/requestHits.py
@@ -81,6 +81,21 @@ for k in range(unique_ip_len):
             outlier_x.append(t_list[h])
     print len(outlier), len(outlier_x)
 
+    # tr = np.array(tr,dtype = np.float32)
+
+    #For median absolute deviation
+
+    # median_tr = np.median(tr,axis=0)
+    # ref_data = np.sum((tr - median_tr)**2, axis=-1)
+    # ref_data = np.sqrt(ref_data)
+    #med_data = np.median(ref_data) 
+    # ref_pt = 0.6745*ref_data/med_data
+    # tr[ref_pt>3.5] = np.nan
+
+    mad_data = 1.4296 * np.median(np.abs(tr - np.median(tr,None)),None)
+    ref = np.abs(tr-np.median(tr))/mad_data
+    print ref
+
     #plt.figure(figsize = ( ,250))
     # print tr
     plt.plot(t_list,tr)
@@ -91,5 +106,25 @@ for k in range(unique_ip_len):
     # mng.full_screen_toggle()
     plt.show()
 
+    #Using iqr on mad data
+    # q75, q25 = np.percentile(ref, [75 ,25])
+    # iqr = q75 - q25
+    # ul = q75 + 3.0*iqr
+    # ll = q25 - 3.0*iqr
+
+    # print q75,q25
+
+    # r_outlier = []
+    # r_outlier_x = []
+    
+    # for h in range(len(ref)):
+    #     if ref[h] < ll or ref[h] > ul:
+    #         r_outlier.append(ref[h])
+    #         r_outlier_x.append(t_list[h])
+    # print len(r_outlier), len(r_outlier_x)
+    
+    plt.plot(t_list,ref)
+    # plt.plot(r_outlier_x,r_outlier,linestyle = '-',marker = 'o',color = 'r')
+    plt.show()
 #Close file
 fileObject.close()
